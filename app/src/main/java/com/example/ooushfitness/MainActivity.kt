8b57fc45be87802ec2b5046be25300be8d8e9f8d
Alex Green <alex.green@openathens.net>
2023-05-25 16:34:49 +0100

    add dark mode logotype to toolbar

--- app/src/main/java/com/example/ooushfitness/MainActivity.kt
+++ app/src/main/java/com/example/ooushfitness/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.ooushfitness
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.ooushfitness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root);
 
         setSupportActionBar(binding.toolbar)
 
